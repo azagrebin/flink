f9984e15099a96d75d4d18a82451cbf81499770e
Andrey Zagrebin <azagrebin@apache.org>
2019-12-06 20:35:08 +0100

    fixup! [FLINK-14189][runtime] Introduce ResourceProfileBudgetManager.

--- flink-runtime/src/test/java/org/apache/flink/runtime/clusterframework/types/ResourceBudgetManagerTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/clusterframework/types/ResourceBudgetManagerTest.java
@@ -19,13 +19,11 @@
 package org.apache.flink.runtime.clusterframework.types;
 
 import org.apache.flink.configuration.MemorySize;
-
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for {@link ResourceBudgetManager}.
@@ -34,49 +32,39 @@ public class ResourceBudgetManagerTest {
 
 	@Test
 	public void testReserve() {
-		ResourceBudgetManager budgetManager = new ResourceBudgetManager(
-			ResourceProfile.newBuilder().setCpuCores(1.0).setTaskHeapMemory(MemorySize.parse(100 + "m")).build());
+		ResourceBudgetManager budgetManager = new ResourceBudgetManager(createResourceProfile(1.0, 100));
 
-		assertTrue(budgetManager.reserve(
-			ResourceProfile.newBuilder().setCpuCores(0.7).setTaskHeapMemory(MemorySize.parse(70 + "m")).build()));
-		assertThat(budgetManager.getAvailableBudget(),
-			is(ResourceProfile.newBuilder().setCpuCores(0.3).setTaskHeapMemory(MemorySize.parse(30 + "m")).build()));
+		assertThat(budgetManager.reserve(createResourceProfile(0.7, 70)), Matchers.is(true));
+		assertThat(budgetManager.getAvailableBudget(), is(createResourceProfile(0.3, 30)));
 	}
 
 	@Test
 	public void testReserveFail() {
-		ResourceBudgetManager budgetManager = new ResourceBudgetManager(
-			ResourceProfile.newBuilder().setCpuCores(1.0).setTaskHeapMemory(MemorySize.parse(100 + "m")).build());
+		ResourceBudgetManager budgetManager = new ResourceBudgetManager(createResourceProfile(1.0, 100));
 
-		assertFalse(budgetManager.reserve(
-			ResourceProfile.newBuilder().setCpuCores(1.2).setTaskHeapMemory(MemorySize.parse(120 + "m")).build()));
-		assertThat(budgetManager.getAvailableBudget(),
-			is(ResourceProfile.newBuilder().setCpuCores(1.0).setTaskHeapMemory(MemorySize.parse(100 + "m")).build()));
+		assertThat(budgetManager.reserve(createResourceProfile(1.2, 120)), Matchers.is(false));
+		assertThat(budgetManager.getAvailableBudget(), is(createResourceProfile(1.0, 100)));
 	}
 
 	@Test
 	public void testRelease() {
-		ResourceBudgetManager budgetManager = new ResourceBudgetManager(
-			ResourceProfile.newBuilder().setCpuCores(1.0).setTaskHeapMemory(MemorySize.parse(100 + "m")).build());
+		ResourceBudgetManager budgetManager = new ResourceBudgetManager(createResourceProfile(1.0, 100));
 
-		assertTrue(budgetManager.reserve(
-			ResourceProfile.newBuilder().setCpuCores(0.7).setTaskHeapMemory(MemorySize.parse(70 + "m")).build()));
-		assertTrue(budgetManager.release(
-			ResourceProfile.newBuilder().setCpuCores(0.5).setTaskHeapMemory(MemorySize.parse(50 + "m")).build()));
-		assertThat(budgetManager.getAvailableBudget(),
-			is(ResourceProfile.newBuilder().setCpuCores(0.8).setTaskHeapMemory(MemorySize.parse(80 + "m")).build()));
+		assertThat(budgetManager.reserve(createResourceProfile(0.7, 70)), Matchers.is(true));
+		assertThat(budgetManager.release(createResourceProfile(0.5, 50)), Matchers.is(true));
+		assertThat(budgetManager.getAvailableBudget(), is(createResourceProfile(0.8, 80)));
 	}
 
 	@Test
 	public void testReleaseFail() {
-		ResourceBudgetManager budgetManager = new ResourceBudgetManager(
-			ResourceProfile.newBuilder().setCpuCores(1.0).setTaskHeapMemory(MemorySize.parse(100 + "m")).build());
+		ResourceBudgetManager budgetManager = new ResourceBudgetManager(createResourceProfile(1.0, 100));
 
-		assertTrue(budgetManager.reserve(
-			ResourceProfile.newBuilder().setCpuCores(0.7).setTaskHeapMemory(MemorySize.parse(70 + "m")).build()));
-		assertFalse(budgetManager.release(
-			ResourceProfile.newBuilder().setCpuCores(0.8).setTaskHeapMemory(MemorySize.parse(80 + "m")).build()));
-		assertThat(budgetManager.getAvailableBudget(),
-			is(ResourceProfile.newBuilder().setCpuCores(0.3).setTaskHeapMemory(MemorySize.parse(30 + "m")).build()));
+		assertThat(budgetManager.reserve(createResourceProfile(0.7, 70)), Matchers.is(true));
+		assertThat(budgetManager.release(createResourceProfile(0.8, 80)), Matchers.is(false));
+		assertThat(budgetManager.getAvailableBudget(), is(createResourceProfile(0.3, 30)));
+	}
+
+	private static ResourceProfile createResourceProfile(double cpus, int memory) {
+		return ResourceProfile.newBuilder().setCpuCores(cpus).setTaskHeapMemory(MemorySize.parse(memory + "m")).build();
 	}
 }
