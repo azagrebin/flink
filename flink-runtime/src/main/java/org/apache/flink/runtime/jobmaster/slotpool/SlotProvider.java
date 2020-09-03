887f2e49241cc6eb8087499a8c65746c4037218b
Andrey Zagrebin <azagrebin@apache.org>
2020-09-03 12:22:00 +0200

    [hotfix] Do not mix BulkSlotProvider into SlotProvider/SchedulerImpl

--- flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/slotpool/SlotProvider.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/slotpool/SlotProvider.java
@@ -20,7 +20,6 @@ package org.apache.flink.runtime.jobmaster.slotpool;
 
 import org.apache.flink.api.common.time.Time;
 import org.apache.flink.runtime.clusterframework.types.SlotProfile;
-import org.apache.flink.runtime.concurrent.ComponentMainThreadExecutor;
 import org.apache.flink.runtime.instance.SlotSharingGroupId;
 import org.apache.flink.runtime.jobmanager.scheduler.ScheduledUnit;
 import org.apache.flink.runtime.jobmaster.LogicalSlot;
@@ -28,7 +27,6 @@ import org.apache.flink.runtime.jobmaster.SlotRequestId;
 
 import javax.annotation.Nullable;
 
-import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -42,16 +40,7 @@ import java.util.concurrent.CompletableFuture;
  *         fulfilled as soon as a slot becomes available.</li>
  * </ul>
  */
-public interface SlotProvider extends BulkSlotProvider {
-
-	/**
-	 * Starts the slot provider by initializing the main thread executor.
-	 *
-	 * @param mainThreadExecutor the main thread executor of the job master
-	 */
-	default void start(ComponentMainThreadExecutor mainThreadExecutor) {
-		throw new UnsupportedOperationException("Not properly implemented.");
-	}
+public interface SlotProvider {
 
 	/**
 	 * Allocating slot with specific requirement.
@@ -62,11 +51,13 @@ public interface SlotProvider extends BulkSlotProvider {
 	 * @param allocationTimeout after which the allocation fails with a timeout exception
 	 * @return The future of the allocation
 	 */
-	CompletableFuture<LogicalSlot> allocateSlot(
+	default CompletableFuture<LogicalSlot> allocateSlot(
 		SlotRequestId slotRequestId,
 		ScheduledUnit scheduledUnit,
 		SlotProfile slotProfile,
-		Time allocationTimeout);
+		Time allocationTimeout) {
+		throw new UnsupportedOperationException("Not properly implemented.");
+	}
 
 	/**
 	 * Allocating batch slot with specific requirement.
@@ -77,9 +68,9 @@ public interface SlotProvider extends BulkSlotProvider {
 	 * @return The future of the allocation
 	 */
 	default CompletableFuture<LogicalSlot> allocateBatchSlot(
-		SlotRequestId slotRequestId,
-		ScheduledUnit scheduledUnit,
-		SlotProfile slotProfile) {
+			SlotRequestId slotRequestId,
+			ScheduledUnit scheduledUnit,
+			SlotProfile slotProfile) {
 		throw new UnsupportedOperationException("Not properly implemented.");
 	}
 
@@ -103,30 +94,18 @@ public interface SlotProvider extends BulkSlotProvider {
 	}
 
 	/**
-	 * Allocates a bulk of physical slots. The allocation will be completed
-	 * normally only when all the requests are fulfilled.
-	 *
-	 * @param physicalSlotRequests requests for physical slots
-	 * @param timeout indicating how long it is accepted that the slot requests can be unfulfillable
-	 * @return future of the results of slot requests
-	 */
-	default CompletableFuture<Collection<PhysicalSlotRequest.Result>> allocatePhysicalSlots(
-		Collection<PhysicalSlotRequest> physicalSlotRequests,
-		Time timeout) {
-		throw new UnsupportedOperationException("Not properly implemented.");
-	}
-
-	/**
 	 * Cancels the slot request with the given {@link SlotRequestId} and {@link SlotSharingGroupId}.
 	 *
 	 * @param slotRequestId identifying the slot request to cancel
 	 * @param slotSharingGroupId identifying the slot request to cancel
 	 * @param cause of the cancellation
 	 */
-	void cancelSlotRequest(
-		SlotRequestId slotRequestId,
-		@Nullable SlotSharingGroupId slotSharingGroupId,
-		Throwable cause);
+	default void cancelSlotRequest(
+			SlotRequestId slotRequestId,
+			@Nullable SlotSharingGroupId slotSharingGroupId,
+			Throwable cause) {
+		throw new UnsupportedOperationException("Not properly implemented.");
+	}
 
 	/**
 	 * Cancels the slot request with the given {@link SlotRequestId}. If the request is already fulfilled
